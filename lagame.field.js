37b9d82260374ca1de45c147146309b8bb1092e3
Jannik Schürg <jannschu@gmail.com>
2009-12-02 18:36:57 +0100

    add gpl text & gpl logo file

--- lagame.field.js
+++ lagame.field.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of La Game.
+ * Copyright 2009 Shahriar Heidrich and Jannik Schürg
+ * 
+ * La Game is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * La Game is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with La Game.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 
 /* ************************************************************************ *
  *  YOU     ARE     ENTERING     THE     REALM     OF     CHAOS             *
